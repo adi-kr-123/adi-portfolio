968312bfd1d73762b1e61271ae5f06d2e0b4d6ad
ADITYA KUMAR <109411325+adi-kr-123@users.noreply.github.com>
2024-02-20 23:48:57 +0530

    portfolio

---
+++ src/components/Works.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SectionWrapper } from '../hoc'
+
+const Works = ()  => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default SectionWrapper(Works, "")
